eb66be229a35a31b11c2df4fcc46497ac4568f50
Alex Zakharov <azakharau@gmail.com>
2023-12-21 19:22:26 +0300

    Added CORS_ORIGIN_WHITELIST

--- billsappbackend/settings.py
+++ billsappbackend/settings.py
@@ -30,6 +30,15 @@ DEBUG = True
 ALLOWED_HOSTS = ['*', ]
 
 
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
+)
+
 # Application definition
 
 INSTALLED_APPS = [
